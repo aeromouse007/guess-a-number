1db9cfbc6074f0bbe3f9a1f6c1848fa94f3391d2
aeromouse007 <aeromouse007@gmail.com>
2022-10-07 11:38:23 +0800

    give count times

--- guess-a-number.py
+++ guess-a-number.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1
 	num = input('give a number:')
 	num = int(num)
 	if num == r:
 		print('賓果')
+		print('這是你猜的第', count,'次')
 		break
 	if num > r:
 		print('比較大')
 	else:
 		print('比較小')
+	print('這是你猜的第', count,'次')
